5611d04657f4c1886d59ca0c3f004d70b096fea2
abhishek <s.abhishek2399@gmail.com>
2025-06-02 13:47:20 +0530

    endpoint from api

--- frontend/src/custom_hooks/usePdfTool.jsx
+++ frontend/src/custom_hooks/usePdfTool.jsx
@@ -1,7 +1,7 @@
 // src/hooks/usePdfTool.js
 import { useState, useCallback } from 'react';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL =  import.meta.env.VITE_API_URL;;
 
 const usePdfTool = (operationName) => {
   const [isLoading, setIsLoading] = useState(false);
